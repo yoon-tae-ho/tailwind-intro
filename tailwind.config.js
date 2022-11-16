4b17b05ce6a000e8a85ea2b4dec0435b598105d1
yoontaeho <pln0302@yonsei.ac.kr>
2022-11-16 18:00:00 +0900

    Section2: navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
+const openVariant = require("./plugins/openVariant");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {},
-  plugins: [],
+  plugins: [openVariant],
 };
