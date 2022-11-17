2ba59fa05e0949a332622506a624987f5e0ab052
yoontaeho <pln0302@yonsei.ac.kr>
2022-11-17 18:16:10 +0900

    Section 3: Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 const openVariant = require("./plugins/openVariant");
+const animationDelay = require("./plugins/animationDelay");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {},
-  plugins: [openVariant],
+  theme: {
+    extend: {
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+      },
+      // class="animate-wavey"
+      animation: {
+        wavey: "wavey 1000ms linear infinite",
+      },
+    },
+  },
+  plugins: [openVariant, animationDelay],
 };
