1cc49450b24b054951496e90092fc212c20c3275
yoontaeho <pln0302@yonsei.ac.kr>
2022-11-19 18:47:50 +0900

    Implement tailwind css light dark mode with toggle button

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const animationDelay = require("./plugins/animationDelay");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
